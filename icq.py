3646040e5796d075f425828132eea4f61359cbf4
Zazaxyz <dek-096@mail.ru>
2021-11-23 22:21:21 +0400

    поигрался с ботом и фильтром парсера

--- icq.py
+++ icq.py
@@ -13,8 +13,10 @@ statusIn = {'main-menu':True,
             'start-find':False
 }
 
-allow_chats = [
-"760598293", #мой айди
+allow_chats = [ #добавлять без @
+'760598293', #мой айди
+#'676358615', #Василий
+
 ]
 
 def change_status(key, val=True):
@@ -26,29 +28,24 @@ def send_allows(bot):
     for chat in allow_chats:
         bot.send_text(chat_id=chat, text='Бот активен! /start -- начать')
 
-def check_allow(bot,event):
-    for x in allow_chats:
-        if event.from_chat == x:
-            return True
-        else:
-            return False
-
 def startup(bot, event):  #обработчик чата
     # change_status('main-menu')
-    if check_allow(bot,event):
+    if event.from_chat in allow_chats:
         #сортировка поиска грузов
+        if(event.text=='/start'):
+            main_menu(bot,event)
         if statusIn['start-find']:
             bot.send_text(chat_id=event.from_chat, text=f'Сортировка по: "{event.text}"')
             go_pars1(bot,event,event.text)
         else:
             if(event.text!='/start'):
-                bot.send_text(chat_id=event.from_chat, text=f'Я не знаю этой команды :( {event.text}')
-        
-        main_menu(bot,event)
+                bot.send_text(chat_id=event.from_chat, text=f'Я не знаю этой команды :(')
+                main_menu(bot,event)
+        # main_menu(bot,event)
     else:
         bot.send_text(chat_id=event.from_chat,
-            text='Недостаточно прав! Обратитесь к @760598293')
-
+            text=f'Недостаточно прав! Обратитесь к @760598293 \n ваш ID: {event.from_chat}')
+            
 # bot.answer_callback_query(
 #             query_id=event.data['queryId'],
 #             text="Была нажата кнопка 1",
@@ -57,73 +54,126 @@ def startup(bot, event):  #обработчик чата
 def main_menu(bot,event):#main-menu
     default_markup = [
             [{"text": "Поиск груза!", "callbackData": "start-find","style": "attention"}],
-            [{"text": "Получить Id", "callbackData": "get-info", "style": "primary"}],
-            
+            [{"text": "Интересное", "callbackData": "get-info", "style": "primary"}],
+            [{"text": "Atrucks.su", "url": "https://www.atrucks.su/carrier/", "style": "attention"}],
+
             ]
     bot.send_text(chat_id=event.from_chat,
-        text='main menu',
-        inline_keyboard_markup=json.dumps(default_markup))
-
+        text='Выбери действие',
+        inline_keyboard_markup=json.dumps(default_markup))         
+    
 def start_find(bot,event): #заупстить поиск
     change_status('start-find')
     start_find_btns = [ 
-            [{"text": "ЛО + СПб", "callbackData": "reg-LO-SPB","style": "attention"}],
-            [{"text": "Моск.Обл + Москва", "callbackData": "reg-LO-SPB", "style": "attention"}],
-            [{"text": "Вернуться в главное меню", "callbackData": "go-main-menu", "style": "primary"}],
+            [{"text": "СПб", "callbackData": "go-spb", "style": "primary"},
+            {"text": "Ленин.обл", "callbackData": "go-lo", "style": "primary"},
+            {"text": "Ижевск", "callbackData": "go-izh", "style": "primary"}],
+
+            [{"text": "Казань", "callbackData": "go-kazan", "style": "primary"},
+            {"text": "Омск", "callbackData": "go-omsk", "style": "primary"},
+            {"text": "Тюмень", "callbackData": "go-tumen", "style": "primary"}],
+
+            [{"text": "Новосиб", "callbackData": "go-novosib", "style": "primary"},
+            {"text": "Уфа", "callbackData": "go-ufa", "style": "primary"},
+            {"text": "Самара", "callbackData": "go-samara", "style": "primary"}],
+
+            # [{"text": "Татарстан", "callbackData": "go-tatar", "style": "primary"}],
+
+            [{"text": "Вернуться в главное меню", "callbackData": "go-main-menu", "style": "attention"}],
     ]
     bot.send_text(chat_id=event.from_chat,
-            text=f'Статус: {statusIn}')
-    bot.send_text(chat_id=event.from_chat,
-            text='Выбери регион или напиши в чат',
+            text='напиши в чат необходимую погрузку или выбери ниже:',
             inline_keyboard_markup=json.dumps(start_find_btns))
-    # startup(bot,event)
 
-def get_info_client(bot,event): # получить инфу для внесение в разрешенные
-    bot.send_text(chat_id=event.from_chat,
-        text = f'твой id: {event.from_chat}')
+
+def get_info_client(bot,event): # получить инфу для внесение в разрешенные, надо сделать как-нибудь подсчет
+    go_pars1(bot,event,city='СПб',interes=1)
+    go_pars1(bot,event,city='Ленинг',interes=1)
+    go_pars1(bot,event,city='МО',interes=1)
+    go_pars1(bot,event,city='Иж',interes=1)
+    go_pars1(bot,event,city='Каз',interes=1)
+    go_pars1(bot,event,city='Омск',interes=1)
+    go_pars1(bot,event,city='Тюмень',interes=1)
+    go_pars1(bot,event,city='Новосиб',interes=1)
+    go_pars1(bot,event,city='уфа',interes=1)
+    go_pars1(bot,event,city='самар',interes=1, l=1)
     go_main_menu(bot,event)
 
-def go_pars1(bot,event, city='None'):
-
-    bot.send_text(chat_id=event.from_chat, text=f'По запросу найдено:')
+def go_pars1(bot,event, city='None', interes=0, l=0):
+    # bot.send_text(chat_id=event.from_chat, text=f'По запросу найдено:')
     mylist = pars_1(city=city)
-    print(mylist)
+    # print(mylist)
     for x in mylist:
-        bot.send_text(chat_id=event.from_chat, text='Вывод:'+x)    
-    bot.send_text(chat_id=event.from_chat, text='Вывод:'+mylist)
-    go_main_menu(bot,event)
+        if interes==0:
+            if mylist[x] != 0:
+                bot.send_text(chat_id=event.from_chat, text=x, 
+                inline_keyboard_markup="{}".format(json.dumps([[
+                            {"text": "Перейти к заказу", "url": f"https://www.atrucks.su/a/{mylist[x]}/"},
+                        ]])))
+            else:
+                bot.send_text(chat_id=event.from_chat, text=x)
+        else:
+            if mylist[x] != 0:
+                bot.send_text(chat_id=event.from_chat, text=x, 
+                inline_keyboard_markup="{}".format(json.dumps([[
+                            {"text": "Перейти к заказу", "url": f"https://www.atrucks.su/a/{mylist[x]}/"},
+                        ]])))
+    if interes == 0:        
+        start_find(bot,event)
+    # if l == 1:        
+    #     start_find(bot,event)
 
 def go_main_menu(bot,event):
-    bot.send_text(chat_id=event.from_chat,
-        text = f'возвращаю в главное меню..')
+    # bot.send_text(chat_id=event.from_chat,
+    #     text = f'возвращаю в главное меню..')
     change_status('main-menu')
     main_menu(bot,event)    
   
 def buttons_answer_cb(bot, event): #btns events
     if event.data['callbackData'] == "start-find":
-        #bot.send_text(chat_id=event.from_chat, text='Вывожу лоты..')
+        print('callbackData=',event.data['callbackData'])
         change_status('start-find')
         start_find(bot,event)
     elif event.data['callbackData'] == "get-info":
+        print('callbackData=',event.data['callbackData'])
         get_info_client(bot,event)
     elif event.data['callbackData'] == "go-main-menu":
+        print('callbackData=',event.data['callbackData'])
         go_main_menu(bot,event)
+
+    elif event.data['callbackData'] == "go-spb":
+        go_pars1(bot,event, city='Спб')
+    elif event.data['callbackData'] == "go-lo":
+        go_pars1(bot,event, city='Ленин')
+    elif event.data['callbackData'] == "go-izh":
+        go_pars1(bot,event, city='Иж')
+    elif event.data['callbackData'] == "go-kazan":
+        go_pars1(bot,event, city='Каз')
+    elif event.data['callbackData'] == "go-omsk":
+        go_pars1(bot,event, city='Омск')
+    elif event.data['callbackData'] == "go-tumen":
+        go_pars1(bot,event, city='Тюмень')
+    elif event.data['callbackData'] == "go-novosib":
+        go_pars1(bot,event, city='Новосиб')
+    elif event.data['callbackData'] == "go-ufa":
+        go_pars1(bot,event, city='уфа')
+    elif event.data['callbackData'] == "go-samara":
+        go_pars1(bot,event, city='самар')
+    # elif event.data['callbackData'] == "go-tatar":
+    #     go_pars1(bot,event, city='татар')
+
+    
+
 
 def main():
 
     bot = Bot(token=TOKEN)
     send_allows(bot)
     bot.dispatcher.add_handler(MessageHandler(callback=startup))  
-    # bot.dispatcher.add_handler(MessageHandler(callback=message_cb))
 
     bot.dispatcher.add_handler(BotButtonCommandHandler(
         callback=start_find, filters=Filter.callback_data("start_find")))
-    # bot.dispatcher.add_handler(BotButtonCommandHandler(
-    #     callback=go_main_menu, filters=Filter.callback_data("go-main-menu")))
     bot.dispatcher.add_handler(BotButtonCommandHandler(callback=buttons_answer_cb))
-
-    # bot.dispatcher.add_handler(BotButtonCommandHandler(
-    #     callback=buttons_answer_cb, filters=Filter.callback_data("buttons_answer_cb")))
 
     bot.start_polling()
 
--- main.py
+++ main.py
@@ -9,10 +9,29 @@ import json
 rs = requests.Session()
 lkeys = []
 lvals = []
-log,pas = 'log', 'pass'
-#хэдер, без него не работает. надо придумать как вытащить из куков  /  вставлять из data.header запроса get
-#возможно не работало из-за типов данных. тут в словаре все строка. из хедера сессии возможно тип другой, проверить опосля...
-
+log,pas = 'log', 'pas'
+allow_destinations = [
+    'Санкт', 'Ленинградская обл',
+    'Москов','Москва',
+    'Сарат','Самар',
+    'уфа',#придумать радиус
+    'Свердлов', 'Екатеринбург',
+    'Татарстан',
+    'Удмурт',
+    'Киров' , 'Перм' ,
+    'Тюмен' , 'Омск' , 'Новосиб'
+]
+redCities = {
+    'Ижевск':'иж',
+    'Санкт-Петербург':'СПб',
+    'Московск':'МО',
+    'Москва':'МСК',
+    'Екатеринбург':'Екб',
+    'Новосибирск':'Новосиб',
+    'Челябинск':'Челяба',
+    'Набережные':'Н.Челны',
+    'Пермь':'Пермь',
+}
 headers = {
     'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0',
     'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
@@ -24,10 +43,10 @@ headers = {
     'Connection': 'keep-alive',
     'Referer': 'https://www.atrucks.su/user/login/'
     }
+
 current_headers = {}
 
-def get_token():
-#Возвращает токен с сайта из куков в формате str
+def get_token():#Возвращает токен с сайта из куков в формате str
     data = rs.get('https://www.atrucks.su/user/login/')
     for x in data.headers:
         if 'Set-Cookie' in x:
@@ -68,38 +87,11 @@ def check_session():
         print('log: Сессия неактивна, нужна авторизация')       
         get_auth() #неуверен нужно ли..
         
-
 #запускает до авторизации.
-check_session()
+# check_session()
 
 #перебор заказчиков в кабинете
 #https://www.atrucks.su/carrier/auctions/
-def check_comps_and_ids():
-    list_name_company = []
-    list_id_company = []
-    content = rs.get('https://www.atrucks.su/carrier/auctions/',headers=headers).text
-    soup = BeautifulSoup(content, 'html.parser')
-    result_name = soup.findAll('span', {'class':'name'})
-    local_step = 0
-    for item_name in result_name:
-        if local_step > 1:#2 индекса пропустить :)
-            #print(item_name.text) # название конторы
-            list_name_company.append(item_name.text)
-        local_step+=1
-
-    result_idc = soup.findAll('span', {'class':'favorite'})
-    for item_idc in result_idc:    
-        item_idc = item_idc.get('data-id')
-        #print(item_idc) # ид конторы( ИЗБРАННОЕ И ОБЩАЯ ПРОПУСКАЕТСЯ !!!! пересчитать списки на всяк случай)
-        list_id_company.append(item_idc)
-
-    # print(len(result_name), len(result_idc)) #на 2 больше, учитывать!
-    # склеиваем 
-    dict_companys = dict(zip(list_id_company,list_name_company)) #ключ сделать айди. Поскольку в парсе значение приходит именно айдишником заказчика
-    # for x in dict_companys:
-    #     print(x,'>>>',dict_companys[x])
-    return dict_companys
-
 def get_ca_and_ids():
     lnc, lic = [], []
 
@@ -118,21 +110,48 @@ def get_ca_and_ids():
         resIds = soup.findAll('span', {'class':'favorite'})
         for item_idc in resIds:
             item_idc = item_idc.get('data-id')
-            #print(item_idc) # ид конторы( ИЗБРАННОЕ И ОБЩАЯ ПРОПУСКАЕТСЯ !!!! пересчитать списки на всяк случай)
             lic.append(item_idc)
         return dict(zip(lic, lnc))
 
-
-check_session()
+check_session()#запускаем
 
 data_comps = get_ca_and_ids()
 
+#посокращать имена городов: Санкт-Петербург - СПБ. Ленинградская обл. - (ЛО), Екатеринбург - Екат ит.д
 
-def send_out(comp_name,id_z, data_load, st_pl, en_pl, st_price, curr, tr_need, tr_need_d, bvstr, comment):
-    print(f'{comp_name,id_z}',data_load,'|',st_pl,'-',en_pl,'|',st_price,curr,'|',tr_need,tr_need_d, '|',bvstr,comment)\
-    #посокращать имена городов: Санкт-Петербург - СПБ. Ленинградская обл. - (ЛО), Екатеринбург - Екат ит.д
+def check_city_in_RC(city):#сокращение :)
+    for x in redCities:   
+        if x.lower() in city.lower():
+            city=redCities[x]
+    return city
 
+def check_city_in_RC_in(city):#сокращение :)
+    req = city
+    for i, x in redCities.items():
+        if city.lower() in x.lower():
+            req = i
+    return req
 
+def check_city_in_allowCities(city): # city is: Московская обл, г Ногинск, Липецкая обл, г Липецк, etc
+    for x in allow_destinations: 
+        if x.lower() in city.lower():
+            return True
+    return False
+
+#перебираем даты
+def gen_datas(data_load):
+    if len(data_load) == 10:# d1 = 'чч.мм.дддд' #10
+        outData = data_load[0:5] #V
+    elif len(data_load) == 16:# d2 = 'чч.мм.дддд чч:мм' #16
+        outData = data_load[0:5]
+    elif len(data_load) == 23:# d3 = 'чч.мм.дддд - чч.мм.дддд' #23
+        tmpd = data_load.split('—')
+        # print(tmpd)
+        outData = tmpd[0][0:5]+'—'+tmpd[1][1:6]
+    elif len(data_load) == 35:# d4 = 'чч.мм.дддд ча:ми - чч.мм.дддд ча:ми' #35  
+        tmpd = data_load.split('—')
+        outData = str(tmpd[0][0:5]+'—'+tmpd[1][1:6])
+    return outData #---------------------------------------------------------------дата погрузки
 
 # пробуем обрабатывать
 def pars_1(city = 'None', per_page = 300): # None - общая по дефолту, не разбирал строку особо, возможн оможно сделать иначе, хз
@@ -140,57 +159,68 @@ def pars_1(city = 'None', per_page = 300): # None - общая по дефолт
     jat = json.loads(data.text) 
     st, sum_auc = 0, 0
     ret_list = []
+    ret_list_link = []
     # перебор лотов
+    # ввод сокращенного варианта в аську. Обработка:
+    # print('Было:',city)
+    city = check_city_in_RC_in(city)
+    # print('стало:',city)
     for lot in jat['lots']:
-    #проверка на встречку
-        bvstr = ''
-        if("wait_for_bids" in lot):
-            bvstr='встречка открыта'
-        else:
-            bvstr='встречки нету'
-        #
-        comp_name = data_comps[str(lot['company_id'])]
-        if 'labels' in lot:
-            comment = lot['labels']
-            comment = str(comment).split(':')
-            comment = comment[-1].split('}')
-            comment = comment[0]
-        else:
-            comment = ''
-        id_z = lot['text_id'] #-------------------------------------------------------------ид заказа
-        data_load = lot['load_range'] #-----------------------------------------------------дата погрузки
-        st_pl = ''.join(lot['origins']) #---------------------------------------------------где грузимся
-        en_pl = lot['destinations'] #-------------------------------------------------------выгрузка
-        st_price = lot['start_price']#------------------------------------------------------стоимость(заказчик)
-        curr = lot['currency'] #------------------------------------------------------------валюта
-        tr_need = lot['transport']['transport:truck_mode']  #-------------------------------требуемый транспорт
-        tr_need_d = lot['transport']['transport:truck_kinds'] #-----------------------------доп к транспорту
-        # print(st_pl, type(st_pl))
         
-        if city == 'None': # все города погрузки
-            sum_auc += 1
-            ret_list.append(str(comp_name) +' > ' + str(id_z) + \
-                ' > ' + str(data_load) + ' > ' + str(st_pl) + \
-                ' > ' + str(en_pl) +' > ' + str(st_price) + \
-                ' > ' + str(curr) +' > ' + str(tr_need) + \
-                ' > ' + str(tr_need_d) +' > ' + str(bvstr) +' > ' + str(comment))
-            # print(ret_list)
-            # send_out(comp_name,id_z, data_load, st_pl, en_pl, st_price, curr, tr_need, tr_need_d, bvstr, comment)
-        elif city.lower() in str(st_pl.lower()):
-            sum_auc += 1
-            ret_list.append(str(comp_name) +' > ' + str(id_z) + \
-                ' > ' + str(data_load) + ' > ' + str(st_pl) + \
-                ' > ' + str(en_pl) +' > ' + str(st_price) + \
-                ' > ' + str(curr) +' > ' + str(tr_need) + \
-                ' > ' + str(tr_need_d) +' > ' + str(bvstr) +' > ' + str(comment))
-            # print(ret_list)
-            # send_out(comp_name,id_z, data_load, st_pl, en_pl, st_price, curr, tr_need, tr_need_d, bvstr, comment)
-    print("По запросу итого аукционов: ",'>>>',sum_auc)
-    return ret_list
+        en_pl = ' '.join(lot['destinations']) 
+        if check_city_in_allowCities( en_pl ):
+            #проверка на встречку
+            bvstr = ''
+            if("wait_for_bids" in lot):
+                bvstr='встречка открыта'
+            else:
+                bvstr='встречка закрыта'
+            #
+            comp_name = data_comps[str(lot['company_id'])]
+            if 'labels' in lot:
+                comment = lot['labels']
+                comment = str(comment).split(':')
+                comment = comment[-1].split('}')
+                comment = comment[0]
+            else:
+                comment = ''
+            id_z = lot['text_id'] #-------------------------------------------------------------ид заказа
+            linkId = lot['id']
+            # data_load = lot['load_range'] #---------------------------------------------------дата погрузки
+            data_load = gen_datas(str(lot['load_range']))
+            st_pl = ''.join(lot['origins']) #---------------------------------------------------где грузимся
+            st_pl = check_city_in_RC(st_pl)
+            en_pl = check_city_in_RC(en_pl)
+            st_price = lot['start_price']#------------------------------------------------------стоимость(заказчик)
+            st_price /= 1000
+            if float(st_price % 1 ) == 0 and st_price != 0:
+                st_price = int(st_price)
+            if st_price == 0 or st_price == '' or st_price == None:
+                st_price = 'Ожидает ставок'
+            if st_price != 'Ожидает ставок':
+                st_price = str(st_price) + ' тыр' 
 
-#предполагаю высокую нагрузку из-за вызова функции вывода.
-# то что спарсили, в словарь, и передовать на вывод словарь. Так вызовем только 1 раз функцию. сделать потом
-# фильтрация выгрузок.....
+            
+            curr = lot['currency'] #------------------------------------------------------------валюта
+            tr_need = lot['transport']['transport:truck_mode']  #-------------------------------требуемый транспорт
+            tr_need_d = lot['transport']['transport:truck_kinds'] #-----------------------------доп к транспорту
+            ret_Str = str(data_load) + \
+                    ' ' + str(st_pl)+'->'+str(en_pl)+ \
+                        ', '+str(st_price)+ ', ' +str(bvstr) + ' ' +\
+                            ' ' + str(tr_need) + '/' + str(tr_need_d) + ' [' + comp_name +'] '+id_z
+            if city == 'None': # все города погрузки
+                sum_auc += 1
+                ret_list.append(ret_Str)
+                ret_list_link.append(int(linkId))
+            elif city.lower() in str(st_pl.lower()):
+                sum_auc += 1
+                ret_list.append(ret_Str)
+                ret_list_link.append(int(linkId))
+            
+            
+    ret_list.append('По запросу итого аукционов: '+str(sum_auc))#вне цикла
+    ret_list_link.append(0)
+    ret_dict = dict(zip(ret_list, ret_list_link))
+    return ret_dict
 
-# print(pars_1(city='москва')) # оно работает :))
-
+# print(pars_1(city='елабуг'))
